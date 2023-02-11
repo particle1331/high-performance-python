1fda850030e05ae90d78b4a0a52714673d9c5355
particle <54845098+particle1331@users.noreply.github.com>
2023-02-12 04:42:46 +0800

    version zero

--- hp/particles.py
+++ hp/particles.py
@@ -1,9 +1,7 @@
 import numpy as np
 
 
-class Particle:
-    __slots__ = ('x', 'y', 'ω') 
-    
+class Particle:    
     def __init__(self, x, y, angular_vel):
         self.x = x
         self.y = y
@@ -12,28 +10,23 @@ class Particle:
 
 class ParticleSimulator:
     def __init__(self, particles: list[Particle], h=1e-5):
-        self.h = h
+        self.h = h  # Euler-method increment 
         self.particles = particles
-        self.data = np.array(
-            [[p.x, p.y, p.ω] for p in particles], dtype=np.float64)
 
     def evolve(self, t: float):
         """Evolve system from t=0 to t=t."""
+        
         n_steps = int(t / self.h)
         for _ in range(n_steps):
-            self.update_data()
+            for p in self.particles:
+                self.update_particle(p)
 
-        for i, p in enumerate(self.particles):
-            p.x, p.y = self.data[i, [0, 1]]
-
-    def update_data(self):
+    def update_particle(self, p: Particle):
         """Evolve particle with Δt = h."""
-        x = self.data[:, [0]]
-        y = self.data[:, [1]]
-        ω = self.data[:, [2]]
-        vx = -y * ω
-        vy =  x * ω
+
+        vx = -p.y * p.ω
+        vy =  p.x * p.ω
         dx = vx * self.h
         dy = vy * self.h
-        self.data[:, [0]] += dx
-        self.data[:, [1]] += dy
+        p.x += dx
+        p.y += dy
